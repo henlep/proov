24677c8e34cfd0158a59e6d998bce4746f2dc4d4
henlep <henrilepik@gmail.com>
2018-02-20 01:23:19 +0200

    spring boot osa

---
+++ Desktop/mahenta/src/main/java/com/example/mahenta/MahentaApplication.java
@@ -0,0 +1,12 @@
+package com.example.mahenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahentaApplication.class, args);
+	}
+}
